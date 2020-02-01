2aa531ba689eb37d1635ab1133aca4e45eda344f
Trevor Ng <trevngwing@gmail.com>
2020-02-01 11:19:43 -0500

    no message

--- Assets/BaseInteraction.cs
+++ Assets/BaseInteraction.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class BaseInteraction : MonoBehaviour {
     public string Scriptname;
+    public bool temp;
     void OnEnable()
     {
         (GetComponent(Scriptname) as MonoBehaviour).enabled = true;
