eba7f740c3b901567b4c36791e24b0f6a6c5c432
Trevor Ng <trevngwing@gmail.com>
2020-03-07 10:46:23 -0500

    sorry

---
+++ Assets/Scripts/DialogueText.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueText : MonoBehaviour
+{
+    public GameObject Text;
+
+    public bool OnScreen;
+
+    public ObjectDataCache ObjectData;
+
+    // Use this for initialization
+    void Start()
+    {
+        Text = GameObject.Find("Text");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        ObjectData = collision.gameObject.GetComponent<ObjectDataCache>();
+
+        Text.GetComponent<TextMeshProUGUI>().text = ObjectData.thisObject.myName + "" + ObjectData.thisObject.myDesc;
+    }
+}
